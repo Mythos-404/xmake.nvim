b03bb1fcf5f8d428a1a90a8666261ac9f41b62c4
Mythos-404 <mythos_404@outlook.com>
2023-08-20 01:11:38 +0800

    :sparkles: Use *plenary.nvim* to check if there is an `xmake.lua` file in the directory so that it can be loaded correctly.

--- lua/xmake/init.lua
+++ lua/xmake/init.lua
@@ -12,13 +12,27 @@ M.config = {
 }
 M.default_config = M.config
 
+local function catalogue_detection()
+	local files = require("plenary.scandir").scan_dir(".", {
+		depth = 1,
+		search_pattern = "xmake.lua",
+	})
+
+	for _, _ in pairs(files) do
+		return true
+	end
+	return false
+end
+
 function M.setup(user_conf)
 	user_conf = user_conf or {}
 	M.config = vim.tbl_deep_extend("keep", user_conf, M.default_config)
 
-	require("xmake.set").init()
-	require("xmake.commnd").init()
-	require("xmake.update_intellisense").init()
+	if catalogue_detection() then
+		require("xmake.set").init()
+		require("xmake.commnd").init()
+		require("xmake.update_intellisense").init()
+	end
 end
 
 return M
