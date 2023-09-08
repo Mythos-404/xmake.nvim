d58271cd3ea2c92248578ff46504e4ebf1e6b067
Mythos-404 <mythos_404@outlook.com>
2023-09-08 22:37:22 +0800

    :bug: Fix save project configuration repeat

--- lua/xmake/autocmd/_save_project_info.lua
+++ lua/xmake/autocmd/_save_project_info.lua
@@ -12,11 +12,14 @@ function M.save()
 		local project_info = require("xmake.project_config").info.target
 		require("xmake.project_config._target").get_targets()
 
-		with(open(config.files_path .. "project_info.json", "w+"), function(reader)
+		with(open(config.files_path .. "project_info.json", "r"), function(reader)
 			local data = reader:read("*a")
 			data = vim.json.decode(#data ~= 0 and data or "{}")
 			data[config.work_dir] = project_info
-			reader:write(vim.json.encode(data))
+
+			with(open(config.files_path .. "project_info.json", "w"), function(reader_)
+				reader_:write(vim.json.encode(data))
+			end)
 		end)
 	end)
 end
