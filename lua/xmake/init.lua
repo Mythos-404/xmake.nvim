3290270c1f399c5a1e72506be61be2df2cf273fe
Mythos-404 <mythos_404@outlook.com>
2023-09-17 05:23:15 +0800

    :sparkles: Plugin provides more detail when launched

--- lua/xmake/init.lua
+++ lua/xmake/init.lua
@@ -29,11 +29,14 @@ function M.setup(user_conf)
 	user_conf = user_conf or {}
 	M.config = vim.tbl_deep_extend("keep", user_conf, M.default_config)
 
-	if catalogue_detection() then
-		require("xmake.set").init()
-		require("xmake.commnd").init()
-		require("xmake.update_intellisense").init()
+	if not catalogue_detection() then
+		require("xmake.log").warn("No `xmake.lua` has stopped loading in this directory")
+		return
 	end
+
+	require("xmake.set").init()
+	require("xmake.commnd").init()
+	require("xmake.update_intellisense").init()
 end
 
 return M
