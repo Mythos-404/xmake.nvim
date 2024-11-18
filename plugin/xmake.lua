970f6ffbb63cfb5fe4ad5bf8ec217ab5ed708b63
Mythos_404 <mythos_404@outlook.com>
2024-11-19 01:41:47 +0800

    feat(start): identify if it's an `xmake` project

--- plugin/xmake.lua
+++ plugin/xmake.lua
@@ -2,12 +2,34 @@ if vim.g.loaded_xmake then return end
 vim.g.loaded_xmake = true
 
 local group = vim.api.nvim_create_augroup("xmake", { clear = true })
+local function is_enable()
+	local __enable = nil
+	return function()
+		if __enable == nil then
+			if vim.system({ "xmake", "show", "--list=targets" }):wait().code == 0 then
+				__enable = true
+			else
+				__enable = false
+			end
+		end
+		return __enable
+	end
+end
 
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
 	group = group,
 	callback = function()
+		if not is_enable()() then return end
 		require("xmake").info.all_defer_reload()
 		require("xmake").command.init()
+	end,
+})
+
+vim.api.nvim_create_autocmd({ "LspAttach" }, {
+	group = group,
+	callback = function(args)
+		if not is_enable()() then return end
+		require("xmake").lsp.init(args)
 	end,
 })
 
@@ -15,6 +37,7 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
 	group = group,
 	pattern = "xmake.lua",
 	callback = function()
+		if not is_enable()() then return end
 		local Xmake = require("xmake")
 		local on_save = Xmake.config.on_save
 		if on_save.reload_project_info then
